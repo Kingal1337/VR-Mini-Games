036ffac1ac44b6e6c10937e77657bea3e5828211
Alan Tsui <Kingal1337@users.noreply.github.com>
2020-03-20 17:15:29 -0400

    -Added URP -Migrated all assets to URP -Created Glow Shader

--- Assets/Scripts/VR Code/ObjectOutline.cs
+++ Assets/Scripts/VR Code/ObjectOutline.cs
@@ -3,38 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class ObjectOutline : MonoBehaviour
-{
-    private Shader shader;
-
-    public Color outlineColor;
-
-    [Range(0,1)]
-    public float outlineWidth;
-
-    public void Start() {
-        shader = Shader.Find("Outlined/Custom");
-        ApplyShader();
-    }
-
-    public void ApplyShader() {
-        if (shader != null) {
-            Renderer renderer = gameObject.GetComponent<Renderer>();
-            Material[] mats = renderer.materials;
-            for (int i = 0; i < mats.Length; i++) {
-                mats[i].shader = shader;
-                mats[i].SetColor("_OutlineColor", outlineColor);
-                mats[i].SetFloat("_Outline", 0f);
-            }
-        }
-    }
+public class ObjectOutline : MonoBehaviour { 
 
     public void AddOutline(XRBaseInteractable xr) {
         Renderer renderer = xr.gameObject.GetComponent<Renderer>();
         if (renderer != null) {
             Material[] mats = renderer.materials;
             for (int i = 0; i < mats.Length; i++) {
-                mats[i].SetFloat("_Outline", outlineWidth);
+                mats[i].SetFloat("_On", 1f);
             }
         }
     }
@@ -44,7 +20,7 @@ public class ObjectOutline : MonoBehaviour
         if (renderer != null) {
             Material[] mats = renderer.materials;
             for (int i = 0; i < mats.Length; i++) {
-                mats[i].SetFloat("_Outline", 0f);
+                mats[i].SetFloat("_On", 0f);
             }
         }
     }
